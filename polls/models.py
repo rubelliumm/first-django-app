77c9e2df2870b2628664fdfcca5ca2d2135cdb85
rubelliumm <ahmedrubel935@gmail.com>
2021-12-21 16:30:25 +0600

    21 dec

--- polls/models.py
+++ polls/models.py
@@ -6,9 +6,9 @@ class Poll(models.Model):
     option_one = models.CharField(max_length=30)
     option_two = models.CharField(max_length=30)
     option_three = models.CharField(max_length=30)
-    option_one_count =models.IntegerField(default=0)
-    option_two_count =models.IntegerField(default=0)
-    option_three_count =models.IntegerField(default=0)
+    option_one_count = models.IntegerField(default=0)
+    option_two_count = models.IntegerField(default=0)
+    option_three_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.question
